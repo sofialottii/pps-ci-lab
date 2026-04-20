023fd31023d9652584b2eaf860d408adbaf4bc37
Sofia <irenesofia.lotti@studio.unibo.it>
2026-04-20 12:28:24 +0200

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,22 @@ plugins {
     scala
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val scalaVersion = "3.8.3"
+    implementation("org.scala-lang:scala3-library_3:$scalaVersion")
+}
+
+tasks.withType<ScalaCompile>().configureEach {
+    scalaCompileOptions.additionalParameters = listOf(
+        "-Xunchecked-java-output-version",
+        JavaVersion.current().majorVersion,
+    )
+}
+
+tasks.withType<AbstractCopyTask>().configureEach {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
